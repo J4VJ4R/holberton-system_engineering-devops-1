5e7e6f38d526241e60344b5a15fc050daf5bd1b1
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-21 13:43:53 +0000

    Zombie commit! (#11)

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * infinite_while - function that let get bussy the main
+ *
+ * Return: Always 0
+ */
 int infinite_while(void)
 {
 	while (1)
@@ -10,21 +15,31 @@ int infinite_while(void)
 	return (0);
 }
 
-void _fork(void) 
-{ 
+/**
+ * _fork - function that create a child process
+ *
+ * Return: Nothing
+ */
+void _fork(void)
+{
 int ret;
 
 	ret = fork();
-	if (ret == 0) 
+	if (ret == 0)
 	{
-		sleep(1); 
+		sleep(1);
 		exit(0);
 	}
 	else
-		printf("Zombie process created, PID: %d\n", ret); 
-} 
+		printf("Zombie process created, PID: %d\n", ret);
+}
 
-int main()
+/**
+ * main - main test function
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
 	_fork();
 	_fork();
