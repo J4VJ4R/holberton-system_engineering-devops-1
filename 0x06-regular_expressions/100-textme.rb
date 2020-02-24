b50a6aa793150d968654978fd2a59ee645a61025
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-02-24 04:12:23 +0000

    Adding advanced grouping control

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 line = ARGV[0]
-sender = line.scan /(\[from:(.?\d*|.?\w*)\])/
-receiver = line.scan /(\[to:(.?\d*|.?\w*)\])/
-flags = line.scan /(flags:((:*-*\d+)*))/
-puts "#{sender[0][1]},#{receiver[0][1]},#{flags[0][1]}"
+# sender = line.scan /(\[from:(.?\d*|.?\w*)\])/
+# receiver = line.scan /(\[to:(.?\d*|.?\w*)\])/
+# flags = line.scan /(flags:((:*-*\d+)*))/
+# puts "#{sender[0][1]},#{receiver[0][1]},#{flags[0][1]}"
+sender = line.scan /(?<=from:).+?(?=\])/
+receiver = line.scan /(?<=to:).+?(?=\])/
+flags = line.scan /(?<=flags:).+?(?=\])/
+puts "#{sender[0]},#{receiver[0]},#{flags[0]}"
