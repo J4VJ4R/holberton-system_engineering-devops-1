2901f9fe37b8060d1676c956683e2cb3309a959d
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-03-24 19:02:06 +0000

    Client configuration file (w/ Puppet), commit!

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Set up your client SSH configuration file so refuse to authenticate using a password
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+}
