ab65cb110acfd1afdb66a3106ec6da81c3a4ce45
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-04 16:25:18 -0500

    Add validation for arguments

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,29 +3,24 @@
 Python script that, using REST API (https://jsonplaceholder.typicode.com/), for
 a given employee ID, returns information about his/her TODO list progress.
 """
-import urllib
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    user = sys.argv[1]
+    if len(sys.argv) >= 2 and sys.argv[1].isdigit():
+        user = sys.argv[1]
+        url = "https://jsonplaceholder.typicode.com/user/" + user + "/todos"
+        repos = requests.get(url)
 
-    if len(sys.argv) < 2 or type(sys.argv[1]) != int:
-        print('Usage: 0-gather_data_from_an_API.py <int>')
-        return
+        first = True
+        done = len([todo for todo in repos.json() if todo.get("completed")])
+        total = len(repos.json())
 
-    url = "https://jsonplaceholder.typicode.com/user/" + user + "/todos"
-    repos = requests.get(url)
-
-    first = True
-    done = len([todo for todo in repos.json() if todo.get("completed")])
-    total = len(repos.json())
-
-    for element in repos.json():
-        if first:
-            print("Employee Ervin Howell is done with tasks({}/{}):".
-                  format(done, total))
-            first = False
-        if element.get("completed"):
-            print("\t {}".format(element.get("title")))
+        for element in repos.json():
+            if first:
+                print("Employee Ervin Howell is done with tasks({}/{}):".
+                    format(done, total))
+                first = False
+            if element.get("completed"):
+                print("\t {}".format(element.get("title")))
