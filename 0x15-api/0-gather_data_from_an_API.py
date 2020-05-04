26676c069b2bd85c8698fbe9e2e6e4a165bea7d5
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-04 16:09:14 -0500

    Gather data from an API, commit!

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script that, using REST API (https://jsonplaceholder.typicode.com/), for
+a given employee ID, returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/user/" + user + "/todos"
+    repos = requests.get(url)
+
+    first = True
+    done = len([todo for todo in repos.json() if todo.get("completed")])
+    total = len(repos.json())
+
+    for element in repos.json():
+        if first:
+            print("Employee Ervin Howell is done with tasks({}/{}):".
+                  format(done, total))
+            first = False
+        if element.get("completed"):
+            print("\t {}".format(element.get("title")))
