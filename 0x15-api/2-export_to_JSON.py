34ce4d26783bc8d4b38e63af22daff48a9f590d0
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-04 19:06:12 -0500

    Incliude the JSON lib

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,11 +4,13 @@ Python script that, using REST API (https://jsonplaceholder.typicode.com/), for
 a given employee ID, returns information about his/her TODO list progress and
 save it in a JSON file.
 """
-import requests
-import sys
 
 
 if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
     if len(sys.argv) >= 2 and sys.argv[1].isdigit():
         user = sys.argv[1]
         url = "https://jsonplaceholder.typicode.com/users/" + user
@@ -18,19 +20,14 @@ if __name__ == "__main__":
         url = "https://jsonplaceholder.typicode.com/users/" + user + "/todos"
         repos = requests.get(url)
 
-        cu = ""
+        todo = {}
+        cu = []
         for element in repos.json():
-            s = '"task": "{}", "completed": "{}", "username": "{}"'.format(
-                element.get("title"),
-                element.get("completed"),
-                username)
-            if cu == "":
-                sep = ""
-            else:
-                sep = ", "
-            cu = cu + sep + "{" + s + "}"
-        cu = '"{}": [{}]'.format(user, cu)
-        cu = '{' + cu + '}'
+            s = {"task": element.get('title'),
+                 "completed": element.get('completed'),
+                 "username": username}
+            cu.append(s)
+        todo[user] = cu
 
         with open('{}.json'.format(user), 'w') as the_file:
-            the_file.write(cu)
+            json.dump(todo, the_file)
